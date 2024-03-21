451b30e93d390137c6e1ff0278ec5ab768e93e55
aporosh <115151056+aporosh@users.noreply.github.com>
2024-03-21 21:24:41 +0300

    react app

---
+++ src/features/products/productsSlice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { BASE_URL } from "../../utils/constants";
+import axios from "axios";
+
+export const getProducts = createAsyncThunk(
+    'products/getProducts', 
+    async (_, thunkAPI) => {
+    try {
+        const res = await axios(`${BASE_URL}/products`);
+        return res.data;
+    } catch(err) {
+        console.log(err);
+        return thunkAPI.rejectWithValue(err)
+    }
+})
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState: {
+        list: [],
+        // filtered: [],
+        // related: [],
+        isLoading: false
+    },
+    extraReducers: (buider) => {
+        buider.addCase(getProducts.pending, (state) => {
+            state.isLoading = true;
+        });
+        buider.addCase(getProducts.fulfilled, (state, { payload }) => {
+            state.list = payload;
+            state.isLoading = false;
+        });
+        buider.addCase(getProducts.rejected, (state) => {
+            state.isLoading = false;
+        });
+    }
+})
+
+export default productsSlice.reducer;
